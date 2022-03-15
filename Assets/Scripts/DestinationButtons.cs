84631a5fded020b3be97c0b7035e9429d5d980bb
Gerald Clark <geraldclarkaudio@gmail.com>
2022-03-15 15:01:49 -0500

    Added GameManager and Homebase Button Functionality

---
+++ Assets/Scripts/DestinationButtons.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PaperKiteStudios.MultiplicationMastermind {
+    public class DestinationButtons : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject planet1Button;
+
+        [SerializeField]
+        private GameObject planet2Button;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (GameManager.Instance.cargoShipHelped == true)
+            {
+                planet1Button.SetActive(true);
+                planet2Button.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PaperKiteStudios.MultiplicationMastermind
+{
+    
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager _instance;
+        public static GameManager Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    Debug.LogError("GameManager is null");
+                }
+                return _instance;
+            }
+        }
+
+        public bool cargoShipHelped { get; set; }
+        public bool planet1Helped { get; set; }
+        public bool planet2Helped { get; set; }
+
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+    }
+}
